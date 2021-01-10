52ca56ece156619ede86ed76c52335af35b27532
Gustavo Trindade <gustavo96634075@gmail.com>
2021-01-10 20:20:31 -0300

    first commit

---
+++ js-introducao/js/importar.js
@@ -0,0 +1,22 @@
+var botaoImportar = document.querySelector("#buscar-pacientes"); 
+
+botaoImportar.addEventListener("click", function(){
+
+    console.log("Buscando pacientes...");
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function() {
+        var resposta = xhr.responseText;
+
+        var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach(function(paciente) {
+            adicionaPacienteNaTabela(paciente);
+        });
+    });
+
+    xhr.send();
+});
